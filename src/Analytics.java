c1619fa73e2d76d4a703d1fb9599dc8ad19de0fc
John Valenti <john.valenti@verint.com>
2017-08-09 15:52:19 +0100

    analytics

--- src/Analytics.java
+++ src/Analytics.java
@@ -1,23 +1,35 @@
+import java.util.ArrayList;
 
 public class Analytics {
 	ScheduleData sd;
 	EnergyData ed;
-	private static double COSTPEREMPLOYEEHOUR = 100; //in pence
+	private static double COSTPEREMPLOYEEHOUR = 0.4; //in pence
+	ArrayList<Double> costings = new ArrayList<Double>();
 	//private static double CONSTANTPRICE = 1000; 
+	int averageservicelevel = 0;
 	
 	public Analytics(ScheduleData sd, EnergyData ed) {
 		this.sd = sd;
 		this.ed = ed;
-		getPrice();
+		setCostings();
 	}
 	
-	public void getPrice() {
+	public void setCostings() {
 		double daycost = 0;
 		for(int i = 0;i<sd.averageday.size();i++) {
-
-			daycost = daycost+ (sd.averageday.get(i)*ed.August.get(i)*COSTPEREMPLOYEEHOUR);
+			costings.add(sd.averageday.get(i)*ed.August.get(i));
+			averageservicelevel = averageservicelevel+ sd.averageday.get(i);
 		}
-		//daycost = daycost + CONSTANTPRICE;
-		System.out.println("the electricity cost for running 1 average day in August = £"+ String.format("%1$,.2f", (daycost/100)));
+		averageservicelevel = averageservicelevel/sd.averageday.size();
 	}
+	//***max hour***
+	//cheapest hour with employees - companies cheapiest hour
+	
+	//***cheapest hour with above average employees***
+	//*cheapest electricity price with non zero employees*
+	//*cheapest electricity price with above average employees*
+	
+	//max hour with below average employees
+	
+	
 }
